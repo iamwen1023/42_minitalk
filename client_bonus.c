24429b97759bf0d1143701170079bd62073be983
Wen-tzu LO <wlo@e1r11p17.42.fr>
2021-08-05 11:17:43 +0200

    sig_sev

--- client_bonus.c
+++ client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: wlo <wlo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/02 10:49:01 by wlo               #+#    #+#             */
-/*   Updated: 2021/08/04 15:41:47 by wlo              ###   ########.fr       */
+/*   Updated: 2021/08/05 11:17:14 by wlo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ int	main(int argc, char *argv[])
 		ft_putstr("Please enter a PID and a message\n");
 		return (1);
 	}
-	act.sa_sigaction = receive_message_service;
+	act.sa_sigaction = sig_serv;
 	act.sa_flags = SA_SIGINFO;
 	sigemptyset(&act.sa_mask);
 	sigaddset(&act.sa_mask, SIGUSR2);
